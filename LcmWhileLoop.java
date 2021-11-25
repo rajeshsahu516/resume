b73c537d6793e42ba0ff951c8b196052601e72e3
rajeshsahu516 <94278675+rajeshsahu516@users.noreply.github.com>
2021-11-25 22:06:58 +0530

    Add files via upload

---
+++ LcmWhileLoop.java
@@ -0,0 +1,18 @@
+package com.tehnoelevate.lcm;
+
+public class LcmWhileLoop {
+
+	public static void main(String[] args) {
+		int n1 = 72,n2 = 120,lcm;
+		lcm = (n1 > n2) ? n1 : n2;
+		while(true) {
+			if(lcm % n1 == 0 && lcm % n2 == 0) {
+				System.out.printf("The LCM of %d and %d.",n1,n2,lcm);
+				break;
+				
+			}
+		++lcm;
+		}
+	}
+
+}
---
+++ MultiplicationTable.java
@@ -0,0 +1,15 @@
+package com.tehnoelevate.multplicationtable;
+
+public class MultiplicationTable {
+
+	public static void main(String[] args) {
+		int num = 5;
+		for(int i=1;i <= 10;++i) {
+			System.out.printf("%d * %d = %d \n",num,i,num * i);
+			
+		}
+
+	}
+}
+
+
